11c9b6604f5f5f8762eb8707fac67091521ee845
hermantretteteig <herman.tretteteig@hotmail.com>
2020-04-26 23:24:52 +0200

    Lagt til ikon for programmet

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
@@ -29,6 +30,8 @@ public class App extends Application {
         stage.setTitle("Datamaskinkonfiguering");
         stage.setResizable(false);
         stage.show();
+        Image ikoner = new Image("ikoner/Gnome-computer.png");
+        stage.getIcons().add(ikoner);
 
 
     }
